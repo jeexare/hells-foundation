b07b9afe14e38a9771e78ac6b7a1614017b87538
jeexare <hgrubiof@gmail.com>
2025-05-21 21:59:27 -0600

    events and links are added from the javascript so they don't have to be changed in both places

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 // Elementos clave del DOM
 const navUser = document.getElementById("nav-user"); // En la navbar, donde sale el icono y nombre de usuario
-const mobileSidebar = document.getElementById("mobile-sidebar"); // La sidebar del telefono 
+const mobileSidebar = document.getElementById("mobile-sidebar"); // La sidebar del telefono
 const overlay = document.getElementById("overlay"); // Fondo oscuro sobre el foro en el telefono
 const vw = window.innerWidth; // Tamaño de la pantalla del navegador (en píxeles)
+const sideLinksDiv = document.getElementById("side-links"); // Links de Navegación en el SideBar
+const announLinksDiv = document.getElementById("announ-links"); // Links de Navegación en el Tablón de Anuncios
+
+const sideEventsDiv = document.getElementById("side-events"); // Eventos en el SideBar
+const announEventsDiv = document.getElementById("announ-events"); // Eventtos en el Tablón de Anuncios
+
+const linksData = `<a href=""><b>01</b> Link</a>
+<a href=""><b>02</b> Link</a>
+<a href=""><b>03</b> Link</a>
+<a href=""><b>04</b> Link</a>
+<a href=""><b>05</b> Link</a>
+<a href=""><b>06</b> Link</a>
+<a href=""><b>07</b> Link</a>
+<a href=""><b>08</b> Link</a>
+<a href=""><b>09</b> Link</a>
+<a href=""><b>10</b> Link</a>
+<a href=""><b>11</b> Link</a>
+<a href=""><b>12</b> Link</a>`;
+
+const eventsData = `
+<a href="" class="my-event">
+<b>00.00</b>
+<span>My money's in that office, right?</span>
+</a>
+<a href="" class="my-event">
+<b>00.00</b>
+<span>My money's in that office, right?</span>
+</a>
+<a href="" class="my-event">
+<b>00.00</b>
+<span>My money's in that office, right?</span>
+</a>
+<a href="" class="my-event">
+<b>00.00</b>
+<span>My money's in that office, right?</span>
+</a>`;
+
+sideLinksDiv.innerHTML = linksData;
+announLinksDiv.innerHTML = linksData;
+
+sideEventsDiv.innerHTML = eventsData;
+announEventsDiv.innerHTML = eventsData;
 
 /**
  * Maneja el clic sobre el usuario en la navbar.
@@ -17,11 +59,11 @@ function handleNavUserClick() {
  * También activa el overlay y bloquea el scroll del cuerpo.
  */
 function showSidebar() {
-    if (!mobileSidebar.classList.contains('active')) {
+    if (!mobileSidebar.classList.contains("active")) {
         console.log("Show Sidebar"); // TODO: Remove console.log
-        mobileSidebar.classList.add('active');
-        overlay.classList.remove('hidden');
-        document.body.classList.add('no-scroll');
+        mobileSidebar.classList.add("active");
+        overlay.classList.remove("hidden");
+        document.body.classList.add("no-scroll");
     }
 }
 
@@ -30,10 +72,10 @@ function showSidebar() {
  * También oculta el overlay y reestablece el scroll del cuerpo.
  */
 function hideSidebar() {
-    if (mobileSidebar.classList.contains('active')) {
+    if (mobileSidebar.classList.contains("active")) {
         console.log("Hide Sidebar"); // TODO: Remove console.log
-        mobileSidebar.classList.remove('active');
-        overlay.classList.add('hidden');
-        document.body.classList.remove('no-scroll');
+        mobileSidebar.classList.remove("active");
+        overlay.classList.add("hidden");
+        document.body.classList.remove("no-scroll");
     }
 }
